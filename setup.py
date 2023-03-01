7ca04f2f1a5cd14bd03b961fa3c78b4afb4b2136
Daniel McKnight <daniel@neongecko.com>
2023-02-28 18:59:39 -0800

    Add version and release automation

--- setup.py
+++ setup.py
@@ -4,17 +4,61 @@ from setuptools import setup
 PLUGIN_ENTRY_POINT = 'ovos-stt-plugin-selene = ovos_stt_plugin_selene:SeleneSTT'
 CONFIG_ENTRY_POINT = 'ovos-stt-plugin-selene.config = ovos_stt_plugin_selene:SeleneSTTConfig'
 
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_stt_plugin_selene',
+                                'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
 setup(
     name='ovos-stt-plugin-selene',
-    version='0.0.1',
+    version=get_version(),
     description='A stt plugin for mycroft using the mycroft selene backend services',
     url='https://github.com/OpenVoiceOS/ovos-stt-plugin-selene',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
     packages=['ovos_stt_plugin_selene'],
-    install_requires=["ovos-backend-client",
-                      "ovos-plugin-manager>=0.0.1"],
+    install_requires=required("requirements.txt"),
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
